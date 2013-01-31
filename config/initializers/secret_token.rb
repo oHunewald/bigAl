9703c9ae4983c1417236fb749b77ec6fe2a30974
ohunewald <o.hunewald@gmail.com>
2013-01-31 13:57:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigAl::Application.config.secret_token = 'c484f315d8b6c200be44ec6181653af3b0b072eff3e467b51a88cb3b256ab4bbae95ad87e37261b5af1ad21faba0abd878a687d3e9c131c28fa9b5e393dd7c98'
