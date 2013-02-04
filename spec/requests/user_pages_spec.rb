09f60037b6cd5d964ff9b8b14b95a9e72b8b4a32
ohunewald <o.hunewald@gmail.com>
2013-02-04 16:51:17 +0100

    Finish user sign in test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,12 +4,26 @@ describe "User pages" do
   
   subject { page }
   
-  describe "profile page" do
-    let(:user) {FactoryGirl.create(:user)}
-    before { visit user_path(user) }
+  
+  describe "sign in" do
     
-    it { should have_selector('h1', text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    before { visit new_user_session_path}
+    
+    describe "with valid information" do
+      let(:user) {FactoryGirl.create(:user)}
+      
+      before do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        
+        click_button "Sign in"
+      end
+      
+      it { should have_selector('li', text: user.name) }
+      it { should have_link('Sign out', href: destroy_user_session_path) }
+      it { should_not have_link('Sign in', href: new_user_session_path) }  
+      it { should_not have_button('Sign in') }
+    end
   end  
 end
 
