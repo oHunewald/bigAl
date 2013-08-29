919f9e2fba9b6c1251fb0d62aaba13d67a4c16b1
oHunewald <o.hunewald@gmail.com>
2013-08-29 16:10:03 +0200

    Delete 20130716130631_add_dilution_to_pool.rb

--- db/migrate/20130716130631_add_dilution_to_pool.rb
+++
@@ -1,8 +0,0 @@
-class AddDilutionToPool < ActiveRecord::Migration
-  def change
-  	add_column :pools, :lib_5ul_low_te_buffer, :float
-  	add_column :pools, :ul_dilution, :integer
-  	add_column :pools, :bioanalyser_qbit, :float
-  	add_column :pools, :tdf_used, :float
-  end
-end
