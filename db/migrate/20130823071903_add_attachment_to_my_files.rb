3c046a47b93ed82f714916210ff717e23accae1b
oHunewald <o.hunewald@gmail.com>
2013-08-29 16:12:44 +0200

    Delete 20130823071903_add_attachment_to_my_files.rb

--- db/migrate/20130823071903_add_attachment_to_my_files.rb
+++
@@ -1,8 +0,0 @@
-class AddAttachmentToMyFiles < ActiveRecord::Migration
-  def change
-  	add_column :myfiles, :attachment_file_name, :string
-  	add_column :myfiles, :attachment_content_type, :string
-  	add_column :myfiles, :attachment_file_size, :integer
-  	add_column :myfiles, :attachment_updated_at, :datetime
-  end
-end
