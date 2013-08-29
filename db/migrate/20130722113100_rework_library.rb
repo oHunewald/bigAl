3b9e8b2cf1702aa9ae917ab4d7296e1c999150c0
oHunewald <o.hunewald@gmail.com>
2013-08-29 16:11:33 +0200

    Delete 20130722113100_rework_library.rb

--- db/migrate/20130722113100_rework_library.rb
+++
@@ -1,9 +0,0 @@
-class ReworkLibrary < ActiveRecord::Migration
-	def change
-		change_table :templates do |t|
-			t.remove :shearing_kit, :integer
-			t.remove :fragment_kit, :integer
-			t.remove :mid_kit, :integer
-		end
-	end
-end
