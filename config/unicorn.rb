a401f50a62c3181aa33aaf615a87bd5b7744fe1d
ohunewald <o.hunewald@gmail.com>
2013-08-30 14:35:21 +0200

    unicorn_corrections

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
-worker_processes 2
-timeout 30
+listen "/tmp/unicorn.bigAl.sock"
+worker_processes 5
+timeout 60
