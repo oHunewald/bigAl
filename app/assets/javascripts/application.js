84b15e2fd29a48dd2c7ee277ec3a41ef2e015301
ohunewald <o.hunewald@gmail.com>
2013-09-02 10:44:48 +0200

    deleted some jquery files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// inside app/assets/javascripts/application.js
+
 //= require jquery
 //= require jquery.ui.all
 //= require jquery_ujs
